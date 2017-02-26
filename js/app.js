ffa8c1fa1243f9a4c5ebbb6fc4e8436721995be1
MaxCCT <maxcct@cantab.net>
2017-02-26 15:43:39 -0600

    Finishes building all core functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 var initialLocs = [
-	{id: "1", name: "Colonia Roma", coords: {lat: 19.4210353, lng: -99.156829}, visible: true},
-	{id: "2", name: "Chapultepec", coords: {lat: 19.4200509, lng: -99.1847189}, visible: true},
-	{id: "3", name: "Xochimilco", coords: {lat: 19.2366455, lng: -99.1507596}, visible: true},
-	{id: "4", name: "Frida Kahlo Museum", coords: {lat: 19.355148, lng: -99.1647136}, visible: true},
-	{id: "5", name: "Condesa", coords: {lat: 19.4149962, lng: -99.170139}, visible: true}
+	{id: "1", name: "Roma Norte", coords: {lat: 19.417910, lng: -99.161575}, visible: true,
+	wikiName: "Colonia Roma", keywords: ["roma", "norte", "colonia", "cuauhtémoc", "cuauhtemoc"]},
+	{id: "2", name: "Chapultepec", coords: {lat: 19.419851, lng: -99.186111}, visible: true,
+	wikiName: "Chapultepec", keywords: ["chapultepec", "bosque", "forest", "park"]},
+	{id: "3", name: "Arena México", coords: {lat: 19.424507, lng: -99.152091}, visible: true,
+	wikiName: "Arena México", keywords: ["arena", "lucha", "libre", "wrestling"]},
+	{id: "4", name: "National Museum of Anthropology", coords: {lat: 19.426001, lng: -99.186462}, visible: true,
+	wikiName: "National Museum of Anthropology (Mexico)", keywords: ["museo", "museum", "antropología", "antropologia", "anthropology"]},
+	{id: "5", name: "Condesa", coords: {lat: 19.415189, lng: -99.175612}, visible: true,
+	wikiName: "Condesa", keywords: ["condesa", "colonia", "cuauhtémoc", "cuauhtemoc", "hipódromo", "hipodromo"]},
+	{id: "6", name: "Zócalo", coords: {lat: 19.432606, lng: -99.133180}, visible: true,
+	wikiName: "Zócalo", keywords: ["zócalo", "zocalo", "plaza", "centro", "center", "centre", "constitución", "constitucion"]},
+	{id: "7", name: "Zona Rosa", coords: {lat: 19.425653, lng: -99.163658}, visible: true,
+	wikiName: "Zona Rosa, Mexico City", keywords: ["zona", "rosa", "colonia", "juárez", "juarez", "gay", "korean", "korea"]}
 ];
 
 var Location = function(loc) {
@@ -12,6 +21,8 @@ var Location = function(loc) {
 	this.coords = ko.observable(loc.coords);
 	this.visible = ko.observable(loc.visible);
 	this.id = ko.observable(loc.id);
+	this.wikiName = ko.observable(loc.wikiName);
+	this.keywords = ko.observableArray(loc.keywords);
 };
 
 
@@ -29,11 +40,15 @@ var ViewModel = function() {
 
 	this.markers = ko.observableArray([]);
 
-	this.filter = ko.observable('');
+	this.infoWindows = ko.observableArray([]);
+
+	this.filter = ko.observable("");
+
+	this.showList = ko.observable(true);
 
 	var apiCall = function(loc) {
 		var wikiRequestURL = ("https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&titles=" +
-						  	  loc.name() + "&callback=wikiCallback");
+						  	  loc.wikiName() + "&callback=wikiCallback");
 		var wikiRequestTimeout = setTimeout(function() {
 			$("#info").text('Failed to obtain Wikipedia links')
 		}, 8000);
@@ -46,7 +61,7 @@ var ViewModel = function() {
 
 	var initMap = (function() {
 	    	self.map = new google.maps.Map(document.getElementById('map'), {
-	    	zoom: 12,
+	    	zoom: 14,
 	    	center: self.centerLoc().coords()
 	    });
 	    self.locList().forEach(function(loc) {
@@ -62,25 +77,60 @@ var ViewModel = function() {
 			});
 			marker.addListener('click', function() {
 				infowindow.open(map, marker);
+				self.infoWindows.push(infowindow);
 				apiCall(loc);
 			});
 		});
 	})(self);
 
+	self.filterMatchKeyword = function(keyword) {
+		filter = self.filter().toLowerCase();
+		console.log(filter)
+		return filter === keyword;
+	};
+
 	self.filterMarkers = ko.computed(function() {
 		if (self.filter() !== '') {
 			self.locList().forEach(function(loc) {
-		    	if (self.filter() === loc.id()) {
-					loc.visible(true);
-				} else {
+				if (!loc.keywords().some(self.filterMatchKeyword)) {
 					loc.visible(false);
+				} else {
+					loc.visible(true);
 				}
+			}, self);
+		} else {
+			self.locList().forEach(function(loc) {
+				loc.visible(true);
 			});
 		}
 	}, self);
 
-	self.toggleVisibility = function(loc) {
-		loc.visible(!loc.visible);
+	self.clickLocation = function(loc) {
+		self.markers().forEach(function(marker) {
+			if (marker.id === loc.id()) {
+				console.log(self.map.center)
+				center = new google.maps.LatLng(loc.coords().lat, loc.coords().lng);
+				self.map.panTo(center);
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+				apiCall(loc);
+			} else {
+				marker.setAnimation(null);
+			}
+		});
+	};
+
+	self.clearSelection = function(loc) {
+		self.markers().forEach(function(marker) {
+			marker.setAnimation(null);
+			$("#info").text("");
+		});
+		self.infoWindows().forEach(function(infoWindow) {
+			infoWindow.close();
+			$("#info").text("");
+		});
+		self.locList().forEach(function(loc) {
+			loc.visible(true);
+		});
 	};
 
 	self.updateMarkers = ko.computed(function() {
@@ -101,29 +151,9 @@ var ViewModel = function() {
 		});
 	}, self);
 
+	self.openCloseList = function() {
+		self.showList() ? self.showList(false) : !self.showList(true);
+	}
 }
 
-
 ko.applyBindings(new ViewModel());
-
-	// var wikiRequestURL = ("https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&titles=" + this.name() + "&callback=wikiCallback");
-	// $.ajax({url: wikiRequestURL, dataType: 'jsonp', success: function(response) {
-	// 	if (response.query.pages[0].extract) {
-	// 		self.info(response.query.pages[0].extract);
-	// 		$("#info").append(response.query.pages[0].extract);
-	// 	} else {
-	// 		self.info("Could not retrieve Wikipedia information");
-	// 	}
-	// }});
-	// console.log(this.info())
-
-	// var wikiRequestURL = ("https://en.wikipedia.org/w/api.php?format=json&formatversion=2&action=query&prop=extracts&exintro=&explaintext=&titles=Mexico&callback=wikiCallback");
-	// var info;
-	// $.ajax({url: wikiRequestURL, dataType: 'jsonp', success: function(response){
-	//     info = response.query.pages[0].extract;               
-	// }});         
-	// function assignInfo() {
-	//     return info;
-	// };
-	// console.log(info)
-
